4f798641cbce8a00a0e4a894ca4b87029e5b45a5
Luiz Eduardo <luizeduardo.ufg@gmail.com>
2021-02-28 21:15:42 -0300

    Added navbar and searchbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App/App.css';
+import './styles/App/navbar.css';
+import './styles/App/searchbar.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        {/* <img className="logo" src="images/logo.svg" alt="logo" /> */}
+        <div className="logo">Logo</div>
+        <nav>
+          <ul className="nav__links">
+            <li><a href="/">Página inicial</a></li>
+            <li><a href="/">Adicionar série</a></li>
+            <li><a href="/">Sobre</a></li>
+          </ul>
+        </nav>
+        <a className="cta" href="/"><button>Contato</button></a>
       </header>
+
+      <main>
+        <div className="search_box">
+          <input className="search_box__search_txt" type="text" name="" placeholder="Digite o nome da série..." />
+          <a className="search_box__search_btn" href="/">
+            <i class="fas fa-search"></i>
+          </a>
+        </div>
+      </main>
     </div>
   );
 }
