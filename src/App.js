3aadf47d6f1d106281570a22b3d3965eee26caf0
Luiz Eduardo <luizeduardo.ufg@gmail.com>
2021-03-03 08:16:05 -0300

    Lifting up state with react + axios get results from api

--- src/App.js
+++ src/App.js
@@ -2,24 +2,51 @@ import './styles/App/App.css';
 import Navbar from './components/Navbar/Navbar';
 import Searchbar from './components/Searchbar/Searchbar';
 import SeriesTable from './components/SeriesTable/SeriesTable';
-import { useState } from 'react';
+import React from 'react';
+import axios from 'axios';
 
-function handleSubmit(event) {
-  event.preventDefault();
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange =this.handleChange.bind(this);
+    this.state = {
+      searchBarQuery: '',
+    };
+  }
 
-function App() {
-  return (
-    <div className="body">
-      <Navbar />
-      <main>
-        <form onSubmit={() => {handleSubmit()}}>
-          <Searchbar />
-        </form>
-        <SeriesTable />
-      </main>
-    </div>
-  );
+  handleChange(e) {
+    this.setState({searchBarQuery: e.target.value});
+  }
+
+  handleClick() {
+    // console.log(this.state.searchBarQuery);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    axios.get(`https://www.episodate.com/api/search?q=${this.state.searchBarQuery}&page=1`)
+      .then(response => {
+        console.log(response.data);
+      });
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <Navbar />
+        <main>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <Searchbar
+              query={this.state.searchBarQuery}
+              onChange={this.handleChange}
+              onClick={this.handleClick.bind(this)}
+            />
+          </form>
+          <SeriesTable />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,12 +1,47 @@
 import './searchbar.css';
+import React from 'react';
 
-export default function Searchbar() {
-  return (
-    <div className="search_box">
-      <input className="search_box__search_txt" type="text" name="" placeholder="Digite o nome da série..." />
-      <a className="search_box__search_btn" href="/">
-        <i class="fas fa-search"></i>
-      </a>
-    </div>
-  );
+export default class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    const {searchBarQuery} = this.props;
+    this.state = {searchBarQuery};
+  }
+
+  handleChange(e) {
+    this.props.onChange(e);
+  }
+
+  handleClick() {
+    this.props.onClick();
+  }
+
+  componentWillReceiveProps(props) {
+    const { searchBarQuery } = props;
+    this.setState({ searchBarQuery });
+  }
+
+  render(){
+    return (
+      <div className="search_box">
+        <input
+          className="search_box__search_txt"
+          type="text"
+          value={this.state.searchBarQuery}
+          onChange={this.handleChange}
+          name="search_bar_query"
+          placeholder="Digite o nome da série..."
+        />
+        <button
+          type="submit"
+          className="search_box__search_btn"
+          href="/"
+          onClick={this.handleClick.bind(this)}
+        >
+          <i className="fas fa-search"></i>
+        </button>
+      </div>
+    );
+  }
 }
